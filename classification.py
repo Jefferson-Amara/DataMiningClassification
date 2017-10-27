40f7f72a154a70c55620fd079b385e42860fb54e
Luis Augusto Toscano <gml.toscano@gmail.com>
2017-10-27 19:32:15 -0200

    Algumas melhorias de código e gráficos adicionados

--- classification.py
+++ classification.py
@@ -2,13 +2,14 @@
 import pandas as pd
 import random as rd
 import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
-rd.seed(a=None)
+from matplotlib.colors import ListedColormap
 
 def readDataFile(filePath):
     # Carregando dados
@@ -30,15 +31,20 @@ def readDataFile(filePath):
     
     return dataSet
 
-def getDataSets(data, trainFactor=0.9):
+def getDataSets(data, trainFactor=0.9, random=True):
     # Tamanho do conjunto de treinamento
     trainSetCount = int(round(len(data) * trainFactor))
-    # Seleciona aleatoriamente o conjunto de treinamento
-    trainSetIndexList = rd.sample(range(0, len(data) - 1), trainSetCount)
+    
+    if random:
+        # Seleciona aleatoriamente o conjunto de treinamento
+        trainSetIndexList = rd.sample(range(0, len(data) - 1), trainSetCount)
+    else:
+        trainSetIndexList = range(0, (trainSetCount - 1))
     
     # Cria o conjunto de treinamento a partir do sorteio aleatório
     trainSet = data.loc[data.index[trainSetIndexList]]
     trainSet = trainSet.reset_index()
+    
     # Cria o conjunto de teste com o restante dos dados
     testSet = data.drop(data.index[trainSetIndexList])
     testSet = testSet.reset_index()
@@ -57,14 +63,17 @@ def convertToYCrCb(dataSet):
         
     return yCrCbDataset
 
+# Carregando arquivo de dados
 data_rgb = readDataFile("database-pele-07.dat")
 
+# Criando tabelas para armazenar resultados dos testes
 model_test_rgb = pd.DataFrame(columns=['L', 'RL', 'NB', 'NN'])
 model_test_ycrcb = pd.DataFrame(columns=['L', 'RL', 'NB', 'NN'])
 
+# ========== Testes ========== #
 for i in range(0, 10):
     # ========== RGB ========== #
-    trainSet_rgb, testSet_rgb = getDataSets(data_rgb)
+    trainSet_rgb, testSet_rgb = getDataSets(data_rgb, random=True)
     
     trainSet_x_rgb = trainSet_rgb[['b', 'g', 'r']]
     trainSet_y_rgb = np.ravel(trainSet_rgb[['class']])
@@ -153,8 +162,64 @@ for i in range(0, 10):
     print({"NB_Score_ycrcb":nb_ycrcb_score})
     print({"NN_Score_ycrcb":nn_ycrcb_score})
     
+# ========== END Testes ========== #
+    
+# ========== Imprime estatísticas dos testes ========== #
+print()
 print(model_test_rgb.describe())
 print(model_test_ycrcb.describe())
+print()
+# ========== END Imprime estatísticas dos testes ========== #
 
-del data_rgb, trainSet_rgb, testSet_rgb, trainSet_x_rgb, trainSet_y_rgb, testSet_x_rgb, testSet_y_rgb, rl_model_rgb
+# ========== Imprimindo gráficos para YCrCb ========== #
+
+data_class_1 = testSet_ycrcb.loc[testSet_ycrcb['class'] == 1]
+data_class_2 = testSet_ycrcb.loc[testSet_ycrcb['class'] == 2]
+
+# Criando malha para delimitar regiões
+grid_space = 0.1
+x1t, x2t = np.meshgrid(np.arange(min(testSet_ycrcb['cr']), max(testSet_ycrcb['cr']), grid_space),
+                       np.arange(min(testSet_ycrcb['cb']), max(testSet_ycrcb['cb']), grid_space))
+
+# Prediz os valores para os pontos da malha
+yt = l_model_ycrcb.predict(np.c_[x1t.ravel(), x2t.ravel()])
+yt = yt.reshape(x1t.shape)
+
+colors = ListedColormap(["#ccccff", "#ccffcc"])
+
+plt.pcolormesh(x1t, x2t, yt, cmap=colors)
+plt.plot(data_class_1['cr'], data_class_1['cb'], 'bs', data_class_2['cr'], data_class_2['cb'], 'g^')
+plt.show()
+
+# Prediz os valores para os pontos da malha
+yt = rl_model_ycrcb.predict(np.c_[x1t.ravel(), x2t.ravel()])
+yt = yt.reshape(x1t.shape)
+
+plt.pcolormesh(x1t, x2t, yt, cmap=colors)
+plt.plot(data_class_1['cr'], data_class_1['cb'], 'bs', data_class_2['cr'], data_class_2['cb'], 'g^')
+plt.show()
+
+# Prediz os valores para os pontos da malha
+yt = nb_model_ycrcb.predict(np.c_[x1t.ravel(), x2t.ravel()])
+yt = yt.reshape(x1t.shape)
+
+plt.pcolormesh(x1t, x2t, yt, cmap=colors)
+plt.plot(data_class_1['cr'], data_class_1['cb'], 'bs', data_class_2['cr'], data_class_2['cb'], 'g^')
+plt.show()
+
+# Prediz os valores para os pontos da malha
+yt = nn_model_ycrcb.predict(np.c_[x1t.ravel(), x2t.ravel()])
+yt = yt.reshape(x1t.shape)
+
+plt.pcolormesh(x1t, x2t, yt, cmap=colors)
+plt.plot(data_class_1['cr'], data_class_1['cb'], 'bs', data_class_2['cr'], data_class_2['cb'], 'g^')
+plt.show()
+
+# ========== END Imprimindo gráficos para YCrCb ========== #
+
+# Liberando memória
+del i, data_rgb, trainSet_rgb, testSet_rgb, trainSet_x_rgb, trainSet_y_rgb, testSet_x_rgb, testSet_y_rgb, rl_model_rgb
 del trainSet_ycrcb, testSet_ycrcb, trainSet_x_ycrcb, trainSet_y_ycrcb, testSet_x_ycrcb, testSet_y_ycrcb, rl_model_ycrcb
+del l_rgb_score, rl_rgb_score, nb_rgb_score, nn_rgb_score
+del l_ycrcb_score, rl_ycrcb_score, nb_ycrcb_score, nn_ycrcb_score
+del data_class_1, data_class_2, grid_space, x1t, x2t, yt, colors
